81e5b9ac0f7a0033424639c1002b3b43b51482dd
Tommy Alhamra <tommy.alhamra@ist.id>
2025-05-24 09:32:05 +0700

    initial projects

---
+++ src/components/Brand/index.tsx
@@ -0,0 +1,15 @@
+// type Props = {
+//     // Define the props for the Brand component here
+//     // For example:
+//     // name: string;
+//     // logoUrl: string;
+//     // onClick: () => void;
+// }
+
+const Brand = () => {
+  return (
+    <div>Brand</div>
+  )
+}
+
+export default Brand
